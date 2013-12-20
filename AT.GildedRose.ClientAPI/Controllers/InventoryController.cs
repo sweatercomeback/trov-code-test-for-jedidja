4d1bf4bc05c5e044ae5cc38d3c63a1ea8e637739
Adam Tumminaro <adam@tumminaro.com>
2013-12-20 17:54:14 -0600

    Added Inventory Controller

---
+++ AT.GildedRose.ClientAPI/Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AT.GildedRose.Business;
+
+namespace AT.GildedRose.ClientAPI.Controllers
+{
+    public class InventoryController : ApiController
+    {
+        private IInventoryService _service;
+
+        public InventoryController()
+        {
+        }
+
+        public InventoryController(IInventoryService service)
+        {
+            _service = service;
+        }
+        // GET api/values
+        public IEnumerable<IInventoryItem> Get()
+        {
+            return _service.GetAllItems();
+        }
+
+    }
+}
